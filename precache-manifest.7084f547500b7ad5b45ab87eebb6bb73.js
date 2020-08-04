d46a59a6cb0d1442baeafe4e91c6a2d24f8e8b66
Manoj Kumar <manojkumar5251@gmail.com>
2020-08-04 22:51:23 +0530

    Updates

---
+++ precache-manifest.7084f547500b7ad5b45ab87eebb6bb73.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d5e3f2c4edc2ec211538c2d7b30bf0",
+    "url": "/weew-ui/index.html"
+  },
+  {
+    "revision": "80d13e5de9d97864ead3",
+    "url": "/weew-ui/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "017298fcf1255614b237",
+    "url": "/weew-ui/static/css/main.da2b8cd4.chunk.css"
+  },
+  {
+    "revision": "80d13e5de9d97864ead3",
+    "url": "/weew-ui/static/js/2.b3fb1b9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weew-ui/static/js/2.b3fb1b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "017298fcf1255614b237",
+    "url": "/weew-ui/static/js/main.ec75e682.chunk.js"
+  },
+  {
+    "revision": "030fc83abe08a50cb1f0",
+    "url": "/weew-ui/static/js/runtime-main.12794e28.js"
+  },
+  {
+    "revision": "d6dee1c98e9952eba34254b90707e706",
+    "url": "/weew-ui/static/media/4 How it works.d6dee1c9.png"
+  },
+  {
+    "revision": "3c2b67aa70753aa31cc62c220c97c4f3",
+    "url": "/weew-ui/static/media/Bitmap_k_pattern.3c2b67aa.png"
+  },
+  {
+    "revision": "adfea7232ab6cba7d18130349fa2ecf7",
+    "url": "/weew-ui/static/media/Bitmap_kw_pattern.adfea723.png"
+  },
+  {
+    "revision": "2675b337c32ee04a40c8d352974205c6",
+    "url": "/weew-ui/static/media/Bitmap_kx_pattern.2675b337.png"
+  },
+  {
+    "revision": "9cb072f925db4825c2b00b47baf063ae",
+    "url": "/weew-ui/static/media/Mask_gr.9cb072f9.png"
+  },
+  {
+    "revision": "8007bb9936320d8b2b3519328953af42",
+    "url": "/weew-ui/static/media/Mask_gw.8007bb99.png"
+  },
+  {
+    "revision": "25ce7452c77323c8ac61c6a1b9065bc3",
+    "url": "/weew-ui/static/media/Mask_gz.25ce7452.png"
+  },
+  {
+    "revision": "ee8a57ca3e21237a5700579399378292",
+    "url": "/weew-ui/static/media/Path 4.ee8a57ca.png"
+  },
+  {
+    "revision": "305d11c1cdbd35f84abcca40c5188e93",
+    "url": "/weew-ui/static/media/aiony_haust_1096642_unsplash_k_pattern.305d11c1.png"
+  },
+  {
+    "revision": "5deba4a9eecd9927b959c2848077f0dd",
+    "url": "/weew-ui/static/media/carousal0.5deba4a9.png"
+  },
+  {
+    "revision": "d87ed07a3edde2d6bddcbd18e863efa8",
+    "url": "/weew-ui/static/media/carousal1.d87ed07a.png"
+  },
+  {
+    "revision": "bdd00baf6f77ae5cc34a89bf99e23a42",
+    "url": "/weew-ui/static/media/carousal2.bdd00baf.png"
+  }
+]);
